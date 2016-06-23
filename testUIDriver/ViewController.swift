a94191d710309c6cc0b1481525534c88fb638e8c
zhang gongwei <fenglin@yiran.com>
2016-06-23 17:00:53 +0800

    Initial Commit

---
+++ testUIDriver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testUIDriver
+//
+//  Created by zhang gongwei on 16/6/23.
+//  Copyright © 2016年 AIMI Network Technology Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
